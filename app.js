d2e2d6681203fce9b52c911bf279469f1a04dc85
Nova <nileshtelang07n5@gmail.com>
2023-05-14 15:24:02 +0530

    refactoring

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const inputs=document.querySelectorAll(".contact-input");
+const inputs = document.querySelectorAll(".contact-input");
 
-inputs.forEach(ipt=>{
-    ipt.addEventListener("focus",()=>{
-        ipt.parentNode.classList.add("focus");
-        ipt.parentNode.classList.add("not-empty");
-    });
+inputs.forEach((ipt) => {
+  ipt.addEventListener("focus", () => {
+    ipt.parentNode.classList.add("focus");
+    ipt.parentNode.classList.add("not-empty");
+  });
 
-    ipt.addEventListener("blur",()=>{
-        if(ipt.value==""){
-            ipt.parentNode.classList.remove("not-empty");
-        }
-        ipt.parentNode.classList.remove("focus");
-    });
+  ipt.addEventListener("blur", () => {
+    if (ipt.value == "") {
+      ipt.parentNode.classList.remove("not-empty");
+    }
+    ipt.parentNode.classList.remove("focus");
+  });
 });
